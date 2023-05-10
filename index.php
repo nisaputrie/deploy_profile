3997f7845ade0a70fb114a1c5af2d00e0b4a31a8
Annisa Putri 1911522011 <60305899+nisaputrie@users.noreply.github.com>
2023-05-11 05:28:15 +0700

    proyek-profile

---
+++ index.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
+    <style> 
+      img{
+        width: 250px;
+        height: 250px;
+        }
+    </style>
+    <title>Proyek Deploy Portofolio</title>
+  </head>
+  <body class="bg-secondary">
+    <!-- Header -->
+      <div class="row">
+         
+        <div class="col">
+        </div>
+
+        <div class="col-8">
+        <div class="card text-center shadow mb-4">
+            <div class="card-body px-4 py-4">
+                <img src="https://storage.cloud.google.com/bucket-nisa/foto.jpg" class="rounded-circle w-20 border border-5" alt="">
+              <h2 class="mb-0">Annisa Putri</h2>
+              <span class="fw-light d-block mb-3">Cloud Engineer</span>
+              <p class="card-text">Student of Andalas University | Information System</p>
+            </div>
+            <div class="card-footer bg-white fs-3">
+                <a href="https://www.linkedin.com/in/annisa-putri-3b64551a2/" class="text-secondary"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/nisaputrie" class="text-secondary"><i class="fa-brands fa-github"></i></a>
+                <a href="https://www.instagram.com/nisaaputrie/" class="text-secondary"><i class="fa-brands fa-instagram"></i></i></a>
+            </div>
+          </div>
+
+
+         
+          <div class="card shadow mb-4" >
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <div class="row">
+                    <div class="col-4">Email</div>
+                    <div class="col-8 text-end ">annisaputrie2001@gmail.com</div>
+                </div>
+              </li>
+
+              <li class="list-group-item">
+                <div class="row">
+                    <div class="col-4">Phone Number</div>
+                    <div class="col-8 text-end">081215422023</div>
+                </div>
+              </li>
+              <li class="list-group-item">
+                <div class="row">   
+                    <div class="col-12">I am a final year student majoring Information System at Andalas University with passionate at 
+                      web development, backend programming and very motivated to continue to grow and learn new 
+                      skills all about technology. Have a good communication and fast learner which make it better.
+                      </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+            
+          <div class="card shadow mb-4" >
+            <div class="card-skill bg-light fw-bold">Programming Language</div>
+            <div class="card-body">
+                <div class="row row-cols-5 fs-1 text-center">
+           <div class="col"> <i class="devicon-javascript-plain colored"></i></div>
+           <div class="col"> <i class=" devicon-googlecloud-plain colored"></i></div>
+           <div class="col"><i class="devicon-bootstrap-plain colored"></i></div>
+           <div class="col"><i class="devicon-android-plain colored"></i></div>
+           <div class="col"><i class="devicon-php-plain colored"></i></div>
+
+                </div>
+            </div>
+          </div>
+
+
+          <div class="card" >
+            <div class="card-header bg-light fw-bold">Skills</div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <p class="mb-0">Adaptibility and Resilince</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                      </div>
+                </li>
+                <li class="list-group-item">
+                    <p class="mb-0">Conflict Management</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                      </div>
+                </li>
+                <li class="list-group-item">
+                    <p class="mb-0">Critical Thinking</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%"></div>
+                      </div>
+                </li>
+            </ul>
+          </div>
+        </div>
+    
+        <div class="col">
+        </div>
+
+      </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
